dff79c18be0bf3fb640e02d67b8e7338d95dd81a
dungdo-wizeline <86394154+dungdo-wizeline@users.noreply.github.com>
2022-06-10 14:39:47 +0700

    init repo

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.project}-${var.environment}"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+  capacity_providers = ["FARGATE", ]
+}
