fd675bb2456a6d2d6f05361459bb28ed16aa197f
dungdo-wizeline <86394154+dungdo-wizeline@users.noreply.github.com>
2022-06-29 14:36:20 +0700

    update s3 backend

--- terraform/fargate/main.tf
+++ terraform/fargate/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket  = "demo-tf-backend-store"
+    encrypt = true
+    key     = "terraform.tfstate"
+    region  = "eu-central-1"
+    # dynamodb_table = "terraform-state-lock-dynamo" - uncomment this line once the terraform-state-lock-dynamo has been terraformed
+  }
+}
+
 module "vpc" {
   source             = "./modules/vpc"
   name               = var.name
