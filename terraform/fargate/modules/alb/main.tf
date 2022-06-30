dfa1a685e13ddcd35e18ad0d09ce08777ffaaf3c
dungdo-wizeline <86394154+dungdo-wizeline@users.noreply.github.com>
2022-06-30 10:40:05 +0700

    remove https

--- terraform/fargate/modules/alb/main.tf
+++ terraform/fargate/modules/alb/main.tf
@@ -43,30 +43,25 @@ resource "aws_alb_listener" "http" {
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = 443
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
   }
 }
 
 # Redirect traffic to target group
-resource "aws_alb_listener" "https" {
-    load_balancer_arn = aws_lb.main.id
-    port              = 443
-    protocol          = "HTTPS"
+# resource "aws_alb_listener" "https" {
+#     load_balancer_arn = aws_lb.main.id
+#     port              = 443
+#     protocol          = "HTTPS"
 
-    ssl_policy        = "ELBSecurityPolicy-2016-08"
-    certificate_arn   = var.alb_tls_cert_arn
+#     ssl_policy        = "ELBSecurityPolicy-2016-08"
+#     certificate_arn   = var.alb_tls_cert_arn
 
-    default_action {
-        target_group_arn = aws_alb_target_group.main.id
-        type             = "forward"
-    }
-}
+#     default_action {
+#         target_group_arn = aws_alb_target_group.main.id
+#         type             = "forward"
+#     }
+# }
 
 output "aws_alb_target_group_arn" {
   value = aws_alb_target_group.main.arn
